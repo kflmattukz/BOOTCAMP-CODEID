347bb7aa5d2fd6b0edd24548311c0447d19083eb
Sulkifli Asmunandar <kflmattukz@gmail.com>
2022-09-20 09:45:50 +0800

    little fix

--- Javascript/Day01/quiz/12-getDays.js
+++ Javascript/Day01/quiz/12-getDays.js
@@ -6,6 +6,11 @@ function getDays(month,year){
   if(isNaN(year)) return 'inputan tahun harus dalam integer'
   
   const d = new Date(year, month, 0).getDate();
+  
+  //console.log(new Date(year, month, 0).toLocaleDateString())
+  //console.log(d)
+  //console.log(0 == year % 4 ,0 != year % 100 ,0 == year % 400)
+  
     if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
         return `This month has ${d} days, ${year} adalah tahun kabisat`;
     } else {
@@ -18,3 +23,4 @@ console.log(getDays("2","year")); //inputan tahun harus dalam integer
 console.log(getDays("m","year"));//inputan bulan & tahun harus dalam integer
 console.log(getDays(2,2000)); //This month has 29 days, 2000 adalah tahun kabisat
 console.log(getDays(8,2021)); //This month has 31 hari
+console.log(getDays(3,1995));
