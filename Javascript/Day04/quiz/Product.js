3411c3e398284d9925789aaec5576477d54fdb97
Sulkifli Asmunandar <kflmattukz@gmail.com>
2022-09-22 11:36:16 +0800

    javascript class & export - import module DAY4

---
+++ Javascript/Day04/quiz/Product.js
@@ -0,0 +1,18 @@
+export default class Product{
+    constructor(prodId,prodName, category, price=0,totalBuy=1){
+
+    }
+
+    setPrice(price){
+
+    }
+
+    setTotalBuy(total){
+
+    }
+
+    toString(){
+    
+    }
+
+}
